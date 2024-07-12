66c230557b65fc38c5a618b1561094fb7a51b4ca
suryxnshsingh <suryxnshsingh@gmail.com>
2024-07-12 12:12:11 +0530

    responsive navbar

--- frontend/src/assets/components/nav/navbar.jsx
+++ frontend/src/assets/components/nav/navbar.jsx
@@ -1,43 +1,55 @@
 import { CgProfile } from "react-icons/cg";
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './navbar.css';
 
 export function Navbar() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const token = localStorage.getItem('token');
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const Logout= () =>{
-    localStorage.clear()
-    navigate("/")
-  }
-  const NavLog = ()=> {
-    if(token){
-      return <div className="navbar-section user-actions">
-        <Link to="/profile">Profile</Link>
-        <div  onClick={Logout} className="logout">Logout</div>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const Logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
+  const NavLog = () => {
+    if (token) {
+      return (
+        <div className="navbar-section user-actions">
+          <Link to="/profile">Profile</Link>
+          <div onClick={Logout} className="logout">Logout</div>
         </div>
+      );
+    } else {
+      return (
+        <>
+          <Link to="/signup">Signup</Link>
+        </>
+      );
     }
-    else return <>
-      <Link to="/signin">Login</Link>
-      <Link to="/signup">Signup</Link>
-    </>
-  }
+  };
 
   return (
     <nav className="navbar">
-      <div className="navbar-section logo" >
-      <h1 onClick={()=>{
-        navigate('/')
-      }}>Eventsy</h1>
+      <div className="navbar-section logo">
+        <h1 onClick={() => navigate('/')}>Eventsy</h1>
+        <div className="menu-icon" onClick={toggleMenu}>
+        &#9776;
+      </div>
       </div>
       
-      <div className="navbar-section user-actions">
-        <Link to="/events">Events</Link>
-        <Link to="/whyeventsy">Why Eventsy</Link>
-        <Link to="/aboutus">About Us</Link>
-        <NavLog/>
+      
+
+      <div className={`navbar-section nav-links ${menuOpen ? 'show' : ''}`}>
+        <Link to="/events" onClick={toggleMenu}>Events</Link>
+        <Link to="/whyeventsy" onClick={toggleMenu}>Why Eventsy</Link>
+        <Link to="/aboutus" onClick={toggleMenu}>About Us</Link>
+        <NavLog />
       </div>
     </nav>
   );
