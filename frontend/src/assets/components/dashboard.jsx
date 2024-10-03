989548f2431fd2912709688ae262a83d24c315cf
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2024-10-03 23:13:20 +0530

    bug fixes

--- frontend/src/assets/components/dashboard.jsx
+++ frontend/src/assets/components/dashboard.jsx
@@ -1,10 +1,5 @@
-import { useNavigate } from "react-router-dom"
-import { Navbar } from "./nav/navbar"
 import Footer from "./footer/footer"
-import { AboutUs } from "./about/aboutus"
-import { WhyEventsy } from "./whyEventsy/whyEventsy"
 import { Landing } from "./landing"
-import { CgEnter } from "react-icons/cg"
 
 export function Dashboard(){
     return <div>
